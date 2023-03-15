f74770ec696fd28e20ef1345d61a62228eafc2b5
changgun-park <pcg0537@gmail.com>
2023-03-16 01:11:16 +0900

    feat: implement reset

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; // number 1-20
+let secretNumber = Math.trunc(Math.random() * 20) + 1; // number 1-20
 
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); // string
@@ -18,6 +16,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347'; // inline style로 적용된다.
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   }
   // when guess is too high
   else if (guess > secretNumber) {
@@ -40,4 +39,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '';
+  document.querySelector('.number').style.width = '';
 });
